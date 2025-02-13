30b5e4ed1bc5363906750cff8861d88e37eac653
Elias Eguizabal <emendoza@applaudostudios.com>
2025-02-13 09:35:03 -0600

    Update sh file

--- tools/entrypoint.sh
+++ tools/entrypoint.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
+# Function to check if su-exec is available
 function checkbin() {
-    type -P su-exec
+    type -P su-exec > /dev/null 2>&1
 }
 
+# Function to run commands as the android user
 function su_mt_user() {
-    su android -c '"$0" "$@"' -- "$@"
+    su android -c '"$@"' -- "$@"
 }
 
+# Ensure the android-sdk-linux directory is owned by the android user
 chown android:android /opt/android-sdk-linux
 
+# Check if su-exec is available and use it for executing the script as android user
 if checkbin; then
     exec su-exec android:android /opt/tools/android-sdk-update.sh "$@"
 else
-    su_mt_user /opt/tools/android-sdk-update.sh ${1}
+    su_mt_user /opt/tools/android-sdk-update.sh "$@"
 fi
-
-
-
-
-
-
-
